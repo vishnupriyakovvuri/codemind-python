2b419f7ec00afabcee68db9d928d79b95df8487d
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-05-22 17:32:33 +0530

    n=int(input()) if n%2==0:     t=n//2 else:     t=(1+n)//2
    for i in range(1,n+1):
        for j  in range(1,n-1):
            #if(j<=t):
            print(j,end="")
            #else:
            #    if(j-t!=t):
              #      print(j-t,end="")
        for j in range(n-3,0,-1):
            print(j,end="")
            
        print("")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -11,7 +11,7 @@ for i in range(1,n+1):
         #else:
         #    if(j-t!=t):
           #      print(j-t,end="")
-    for j in range(1,n-2):
+    for j in range(n-3,0,-1):
         print(j,end="")
         
     print("")
