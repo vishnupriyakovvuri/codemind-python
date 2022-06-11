2d8f205a2ce197b07cdd8da2f1cda550112f356e
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-06-11 15:10:47 +0530

    a=input() b=[] min=100 m=0 k=0 for i in a:     if i==' ':        b.append(k)        k=0     else:           if i in'AEIOUaeiou':               k+=1 b.append(k) for i in b:     if i<min:         min=i for i in b:     if i==min:         m+=1 print(m)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,20 @@
+a=input()
+b=[]
+min=100
+m=0
+k=0
+for i in a:
+    if i==' ':
+       b.append(k)
+       k=0
+    else:
+          if i in'AEIOUaeiou':
+              k+=1
+b.append(k)
+for i in b:
+    if i<min:
+        min=i
+for i in b:
+    if i==min:
+        m+=1
+print(m)        
