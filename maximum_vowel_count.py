4532cb8e90b807abe4178b94743933622ef3f5da
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-06-11 15:17:11 +0530

    a=input() b=[] min=0 m=0 k=0 for i in a:     if i==' ':        b.append(k)        k=0     else:           if i in'AEIOUaeiou':               k+=1 b.append(k) for i in b:     if i>min:         min=i print(min)

---
+++ maximum_vowel_count.py
@@ -0,0 +1,17 @@
+a=input()
+b=[]
+min=0
+m=0
+k=0
+for i in a:
+    if i==' ':
+       b.append(k)
+       k=0
+    else:
+          if i in'AEIOUaeiou':
+              k+=1
+b.append(k)
+for i in b:
+    if i>min:
+        min=i
+print(min)        
