0deab98778d994b160d7dc43bb6dcbcbfcc682f1
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-06-28 21:41:55 +0530

    a=int(input()) b=a*a c=0 d=0 while(a>0):     r=a%10     s=b%10     if(r==s):         c+=1     a=a//10     b=b//10     d+=1 if(c==d):     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -2,15 +2,15 @@ a=int(input())
 b=a*a
 c=0
 d=0
-while a>0:
+while(a>0):
     r=a%10
-    p=b%10
-    if(r==p):
+    s=b%10
+    if(r==s):
         c+=1
-    d+=1    
     a=a//10
     b=b//10
-if c==d:
+    d+=1
+if(c==d):
     print('Automorphic Number')
 else:
     print('Not an Automorphic Number')
