77171810a41871d13d617a20dca6be79f9d5cac7
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-06-28 22:05:47 +0530

    def ugly(n):     if(n==1):         return 1     elif(n%2==0):         return ugly(n//2)     elif(n%3==0):         return ugly(n//3)     elif(n%5==0):         return ugly(n//5)     else:         return 0 a=int(input()) if(ugly(a)==1):     print('Ugly Number') else:     print('Not Ugly Number')

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,15 +1,16 @@
 def ugly(n):
-    if n==1:
+    if(n==1):
         return 1
-    elif n%2==0:
+    elif(n%2==0):
         return ugly(n//2)
-    elif n%3==0:
+    elif(n%3==0):
         return ugly(n//3)
-    elif n%5==0:
+    elif(n%5==0):
         return ugly(n//5)
-    return 0;
-n=int(input())
-if ugly(n)==1:
+    else:
+        return 0
+a=int(input())
+if(ugly(a)==1):
     print('Ugly Number')
 else:
     print('Not Ugly Number')
