c2f11bb3273ca0a088a1ca30448ad2bc9f75373b
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-06-29 15:49:14 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] c=0 for i in range(len(a)):     if(a.count(a[i])==1):         b.append(a[i])         c+=1 if(c>0):     print(max(b)) else:     print('-1')

---
+++ UNIQUE_MAXIMUM_NUMBER.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=0
+for i in range(len(a)):
+    if(a.count(a[i])==1):
+        b.append(a[i])
+        c+=1
+if(c>0):
+    print(max(b))
+else:
+    print('-1')
