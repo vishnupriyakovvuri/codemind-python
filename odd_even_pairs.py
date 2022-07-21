584cd69623ca8013d978f4b861b46dcd4a6a466c
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-07-21 14:41:44 +0530

    n=int(input()) o=[] e=[] p=[] a=list(map(int,input().split())) for i in a:     if(i%2==0):         e.append(i)     else:         o.append(i) if(len(e)<=len(o)):     for i in range(len(e)):         p.append(o[i])         p.append(e[i])     for i in range(len(e),len(o)):         p.append(o[i]) else:     for i in range(len(o)):         p.append(o[i])         p.append(e[i])     for i in range(len(o),len(e)):         p.append(e[i]) if len(p)%2!=0:     p.append(0) for i in p:     print(i,end=" ")

---
+++ odd_even_pairs.py
@@ -0,0 +1,26 @@
+n=int(input())
+o=[]
+e=[]
+p=[]
+a=list(map(int,input().split()))
+for i in a:
+    if(i%2==0):
+        e.append(i)
+    else:
+        o.append(i)
+if(len(e)<=len(o)):
+    for i in range(len(e)):
+        p.append(o[i])
+        p.append(e[i])
+    for i in range(len(e),len(o)):
+        p.append(o[i])
+else:
+    for i in range(len(o)):
+        p.append(o[i])
+        p.append(e[i])
+    for i in range(len(o),len(e)):
+        p.append(e[i])
+if len(p)%2!=0:
+    p.append(0)
+for i in p:
+    print(i,end=" ")
